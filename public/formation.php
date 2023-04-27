3c8aba52913ccfa080b84eaa27e7e7d1883b11c0
ratataque <ewanrimmely@outlook.fr>
2023-04-27 18:25:03 +0200

    pro

--- public/formation.php
+++ public/formation.php
@@ -12,16 +12,34 @@
                     <span style="color: darkorange;">-</span> architecture d'un pc, (processeur, ram ...)
                     <span style="color: darkorange;">-</span> algo, bfs, dfs, dijkstra, hash table, binary tree
                 </div>
+            </div>
 
             <div class="p_formation">
                 <div class="titre_apart">BTS SIO     <div class="date">2021/2023</div></div>
                 <div class="pre_formation">
-                    <span style="color: darkorange;">-</span> Developpement D'application web full stack
+                    <span style="color: darkorange;">-</span> Developpement D'application web full stack 
                     <span style="color: darkorange;">-</span> algorithmie (python, C#)
-                    <span style="color: darkorange;">-</span> faille de sécurité webapp et comment les prévenir : sql injection, xss, csrf, lfi
+                    <span style="color: darkorange;">-</span> prévention de faile de sécurité webapp :
+                    sql injection, xss, csrf, lfi
                     <span style="color: darkorange;">-</span> architecture linux, docker, git, vim, apache, nginx
-                    <span style="color: darkorange;">-</span> application
+                    <span style="color: darkorange;">-</span> application en C# tel que calculatrice et demineur
+                    <span style="color: darkorange;">-</span> application mobile en java : sudoku, expertise de voiture,
+                    traking de commande de miel
+                    <span style="color: darkorange;">-</span> langage : python, django, C#, java, php, html, css, javascript, react, sql
+                </div>
+            </div>    
+        </div>
+
+        <div class="pro_cont">
+            <div class="titre_experience underline">Expérience</div>
+            <div class="pro_formation">
+                <div class="titre_apart">Support IT     <div class="date_pro">2021/2023</div></div>
+                <div class="pre_formation">
+                    <span style="color: darkorange;">-</span> Python, C
+                    <span style="color: darkorange;">-</span> architecture d'un pc, (processeur, ram ...)
+                    <span style="color: darkorange;">-</span> algo, bfs, dfs, dijkstra, hash table, binary tree
+                </div>
             </div>
-        </div>    
+        </div>
     </div>
 </div>
