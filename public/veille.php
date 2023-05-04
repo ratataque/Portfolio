f31248c81b11ab52c1eec30846e5acb63aa6dba8
ratataque <ewanrimmely@outlook.fr>
2023-05-04 15:33:57 +0200

    veille ytb

--- public/veille.php
+++ public/veille.php
@@ -3,9 +3,38 @@
     <a class="veille_acceuil button" href="/" onclick="transi(event, 'acceuil');">Acceuil</a>
 
     <div class="veille_content">
-        <div class="titre_experience underline">Google Alerte :</div>
-        Quis hendrerit dolor magna eget est lorem ipsum dolor sit amet, consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada? Id cursus metus aliquam eleifend mi in!
+        <div class="up">
+            <div class="left">
+                <div class="titre_experience underline">Google Actualite :</div>
+                Les Actualitees google sont tres interessantes et efficace pour ce tenir au courant des sujet que nous apprecions
+                <img class="img_veille" src="./images/veille_google.png">
+                <img class="tel_veille" src="./images/veille.jpg">
+            </div>
+            <div class="right">
+                <div class="titre_experience underline">Reddit :</div>
+                <div class="list">
+                    <a href="https://www.reddit.com/r/archlinux/" target="_blank">r/arch</a>
+                    <a href="https://www.reddit.com/r/vim/" target="_blank">r/vim</a>
+                    <a href="https://www.reddit.com/r/DoomEmacs/" target="_blank">r/doomemacs</a>
+                    <a href="https://www.reddit.com/r/linux/" target="_blank">r/linux</a>
+                    <a href="https://www.reddit.com/r/suckless/" target="_blank">r/suckless</a>
+                    <a href="https://www.reddit.com/r/freesoftware/" target="_blank">r/freesoftware</a>
+                    <a href="https://www.reddit.com/r/rust/" target="_blank">r/rust</a>
+                    <a href="https://www.reddit.com/r/golang/" target="_blank">r/golang</a>
+                </div>
+            </div>
+        </div>
 
         <div class="titre_experience underline">Createurs de contenu :</div>
+        <div class="ytb_cont">
+            <img src="./images/liveoverflow.png">
+            <img src="./images/dt.png">
+            <img src="./images/prime.png">
+            <img src="./images/pwnfunction.png">
+            <img src="./images/owasp.png">
+            <img src="./images/joe_grand.png">
+            <img src="./images/John_hammond.png">
+            <img src="./images/tuto.png">
+        </div>
     </div>
 </div>
